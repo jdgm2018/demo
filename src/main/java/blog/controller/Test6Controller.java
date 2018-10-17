b0f7047dd63462ae8c98f34d1df53ddf1bfa9647
jenkins2018@163.com <root@localhost.localdomain>
2018-10-17 15:54:27 +0800

    测试提交

--- src/main/java/blog/controller/Test6Controller.java
+++ src/main/java/blog/controller/Test6Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * 
  * create by Liujishuai on 2015年9月21日
  */
-@Controller("test6Controller")
+@Controller
 @RequestMapping("/test6")
 public class Test6Controller extends BaseController {
 	@Autowired
