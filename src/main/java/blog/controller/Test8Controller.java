46f7b1633cc122f4247fc975caeb08608336acd8
jenkins2018@163.com <root@localhost.localdomain>
2018-10-22 17:06:14 +0800

    测试提交

---
+++ src/main/java/blog/controller/Test8Controller.java
@@ -0,0 +1,26 @@
+package blog.controller;
+
+import blog.service.IUserService;
+import core.controller.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * create by Liujishuai on 2015年9月21日
+ */
+@Controller
+@RequestMapping("/test8")
+public class Test8Controller extends BaseController {
+	@Autowired
+	private IUserService userService;
+
+	@RequestMapping(value = "/test8", method = RequestMethod.GET)
+	public String getByController(String id) {
+		return "hello8";
+	}
+
+
+}
